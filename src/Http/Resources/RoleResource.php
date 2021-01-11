7e22c10a36394cbbd42a5260f18d8d80bc940652
omadonex <omadonex@yandex.ru>
2021-01-11 15:04:21 +0300

    wip

--- src/Http/Resources/RoleResource.php
+++ src/Http/Resources/RoleResource.php
@@ -3,7 +3,7 @@
 namespace Omadonex\LaravelAcl\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
-use Omadonex\LaravelSupport\Traits\TranslateResourceTrait;
+use Omadonex\LaravelLocale\Traits\TranslateResourceTrait;
 
 class RoleResource extends Resource
 {
